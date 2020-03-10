7d1510ead0bbb76ac0b655be21256bd6e67c045b
Delia Popescu <deliabuze@gmail.com>
2020-03-10 13:17:27 +0200

    add letters

--- search.js
+++ search.js
@@ -1,86 +1,20 @@
-const mealsContainer = document.getElementById("meals-container");
-
-const input = document.getElementById("meal-name");
-
-function generateRateUrl(inputValue) {
-    if (inputValue) {
-        return `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
-    }
-}
-
-function clearMealContainer() {
-    mealsContainer.innerHTML = "";
-}
-
-function renderMeals(meals, clean = true) {
-    if (clean) {
-        mealsContainer.innerHTML = "";
-    }
-    for (const meal of meals) {
-        const mealContainer = document.createElement("div");
-        mealContainer.id = "mealContainer";
-        mealContainer.style.marginBottom = "20px";
-
-        const mealName = document.createElement("h2");
-        mealName.id = "mealName";
-        mealName.innerText = meal.strMeal;
-        mealContainer.appendChild(mealName);
-
-        const mealImg = document.createElement("img");
-        mealImg.id = "mealImg";
-        mealImg.setAttribute("src", meal.strMealThumb);
-        mealImg.classList.add("meal-img");
-        mealContainer.appendChild(mealImg);
-
-        const mealDesc = document.createElement("p");
-        mealDesc.id = "mealDesc";
-        mealDesc.innerText = meal.strInstructions;
-        mealContainer.appendChild(mealDesc);
-
-        mealsContainer.appendChild(mealContainer);
-    }
-}
-
-
-const parentDiv = document.createElement("div");
-parentDiv.id = "parentDiv";
-
-for (let i = 65; i < 91; i++) {
+const addLetters = () => {
+  let letterListContainer = document.querySelector("#letters .row");
+  for (let i = 65; i < 91; i++) {
     let c = String.fromCharCode(i);
     const letter = document.createElement("div");
     letter.innerText = c;
     letter.id = c;
-    letter.style.padding = "3px";
-    parentDiv.appendChild(letter);
-}
-document.getElementsByClassName("interactions")[0].appendChild(parentDiv);
+    letter.classList.add("pl-2");
+    letter.classList.add("pr-2");
+    letter.classList.add("zoom");
 
-document.getElementById("parentDiv").addEventListener("click", getLetter);
+    letter.addEventListener("click", getByLetter);
 
-function getLetter(event) {
-    if (event.target !== event.currentTarget) {
-        console.log(event.target.id);
-        const urlLetter = `https://www.themealdb.com/api/json/v1/1/search.php?f=${event.target.id}`;
-        hitSServer(urlLetter);
-    }
-}
-document.getElementById("btn").addEventListener("click", function () {
+    letterListContainer.appendChild(letter);
+  }
+};
 
-    const searchValue = document.getElementById("input").value;
+addLetters();
 
-    const searchUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`;
-    hitSServer(searchUrl);
-
-});
-
-function hitSServer(url) {
-    fetch(url)
-
-        .then(function (response) {
-            return response.json();
-        })
-
-        .then(function (json) {
-            renderMeals(json.meals);
-        });
-}
+document.querySelector('#btn').addEventListener("click", getByName);
